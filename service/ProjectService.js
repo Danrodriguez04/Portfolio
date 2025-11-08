8fd59788d5a96950a66b72c77efc1365f1e9bacf
Danrodriguez04 <danielsantiagorodriguez72@gmail.com>
2025-11-08 21:10:38 +0100

    painting skills and designing section

--- service/ProjectService.js
+++ service/ProjectService.js
@@ -8,7 +8,6 @@ export class ProjectService{
         const headers = new Headers();
         headers.append("content-type", "application/json");
 
-        const generalRoute = 'https://portfol04.netlify.app/';
         const response = await fetch('/assets/projects.json')/*,{
             headers : headers,
             method: 'GET'
