4269c82475484294dc4b81f1094fa888077731a4
Danrodriguez04 <danielsantiagorodriguez72@gmail.com>
2026-05-16 01:28:17 +0200

    prove to send a request to backend

--- service/EmailService.js
+++ service/EmailService.js
@@ -2,7 +2,7 @@
 export class EmailService {
 
     async sendMessage(emailDto){
-        const response = await fetch(`http://localhost:8080/api/email`, {
+        const response = await fetch(`http://api.danielspace.com:8080/EmailSender-0.0.1/api/email`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
